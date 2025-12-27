342e13bb528c8f8760446ea70ed91b6c961d8e55
Victor Crespo <victor_crespo@v2202510305212389262.happysrv.de>
2025-12-27 02:10:20 +0100

    feat: defining the extract and transform of pipeline

---
+++ pipeline_org_pessoal.py
@@ -0,0 +1,22 @@
+import os
+
+import gspread
+from prefect import flow, task
+from tasks import extract_data_from_drive
+from utils import transforme_to_dataframe_pandas
+
+
+@task
+def extract_data():
+    worksheet = extract_data_from_drive()
+    return worksheet
+
+@task
+def transform(worksheet):
+    df = transforme_to_dataframe_pandas(worksheet)
+    return df
+
+@flow(name="etl_exemplo")
+def etl_pipeline():
+    data = extract_data()
+    processed = transforme_to_dataframe_pandas(data)
