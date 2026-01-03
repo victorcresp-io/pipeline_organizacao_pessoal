6be1df5be21c768303033fef9ed812d817354b2b
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-03 23:12:16 +0100

    refactor: alterando a lógica da pipeline e criando funções para extrair dados do banco de dados

--- pipeline_org_pessoal.py
+++ pipeline_org_pessoal.py
@@ -1,9 +1,11 @@
 import os
 
+from google.cloud import bigquery
 from prefect import flow, task
-from tasks import extract_data_from_drive, auth_bigquery, load_df_to_bigquery
+from tasks import extract_data_from_db, auth_bigquery, load_df_to_bigquery
 from utils import transforme_to_dataframe_pandas, cred_bigquery
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -11,16 +13,12 @@ serv_account_sh = os.getenv('SERV_ACCOUNT')
 serv_account_bigquery = os.getenv('SERV_ACCOUNT_BIGQUERY')
 project_id = os.getenv('PROJECT_ID')
 table_id = os.getenv('TABLE_ID')
+path_db_prod = os.getenv('PATH_DB_PROD')
 
 @task
-def extract_data():
-    worksheet = extract_data_from_drive(serv_account_sh)
+def extract_data(path_db_prod):
+    worksheet = extract_data_from_db(path_db_prod)
     return worksheet
-
-@task
-def transform(worksheet):
-    df = transforme_to_dataframe_pandas(worksheet)
-    return df
 
 @task
 def load_to_bigquery(serv_account_bigquery, df, table_id):
@@ -29,8 +27,6 @@ def load_to_bigquery(serv_account_bigquery, df, table_id):
 
 @flow()
 def etl_pipeline():
-    data = extract_data()
-    df = transform(data)
+    df = extract_data(path_db_prod)
     load_to_bigquery(serv_account_bigquery, df, table_id)
 
-etl_pipeline()
--- tasks.py
+++ tasks.py
@@ -1,5 +1,12 @@
+import sqlite3
+import pandas as pd
 from utils import get_cred, open_sheet, select_worksheet, transforme_to_dataframe_pandas, cred_bigquery
 from google.cloud import bigquery
+
+def extract_data_from_db(path):
+    conn = sqlite3.connect(path)
+    df = pd.read_sql('SELECT * FROM despesas', conn)
+    return df
 
 def extract_data_from_drive(serv_account_google_sh):
     cred = get_cred(serv_account_google_sh)
@@ -17,10 +24,16 @@ def auth_bigquery(project_id, serv_account_bigquery):
     return client
 
 def load_df_to_bigquery(client, table_id, df):
+    job_config = bigquery.LoadJobConfig(
+        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
+    )
+
     job = client.load_table_from_dataframe(
         df,
-        table_id
+        table_id,
+        job_config = job_config
     )
 
     job.result()
     print('Upload sucessful!')
+
